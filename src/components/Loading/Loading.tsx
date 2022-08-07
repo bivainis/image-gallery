29fef059b634a8568c1740b4989b461c41c3a62c
Gediminas Bivainis 💜 <gediminas@bivainis.com>
2022-08-07 06:48:20 +0200

    Add Loading component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,11 @@
+import classes from "./Loading.module.scss";
+
+const Loading = () => {
+  return (
+    <div className={classes.Loading}>
+      <p>Loading, please wait...</p>
+    </div>
+  );
+};
+
+export default Loading;
