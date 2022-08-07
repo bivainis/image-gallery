3de05f3492e304aea7807f3b070ae831f1d73176
Gediminas Bivainis 💜 <gediminas@bivainis.com>
2022-08-07 07:36:13 +0200

    Test next page results

--- src/pages/GalleryPage/GalleryPage.test.tsx
+++ src/pages/GalleryPage/GalleryPage.test.tsx
@@ -38,3 +38,44 @@ test("Previous button is disabled on initial page load", async () => {
 
   expect(previousButton).toBeDisabled();
 });
+
+test("Clicking next should load the next page of images", async () => {
+  const user = userEvent.setup();
+  render(<GalleryPage />);
+
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText("Loading, please wait...")
+  );
+
+  const nextButton = await screen.findByLabelText("Next page");
+
+  // @TODO: refactor multiple clicks to by more DRY
+  await user.click(nextButton);
+
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText("Loading, please wait...")
+  );
+
+  await user.click(nextButton);
+
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText("Loading, please wait...")
+  );
+
+  await user.click(nextButton);
+
+  const previousButton = await screen.findByRole("button", {
+    name: "Previous page",
+  });
+
+  const list = await screen.findByRole("list");
+  const { getAllByRole } = within(list);
+  const items = getAllByRole("listitem");
+  const [firstItem] = items;
+  const { getByAltText } = within(firstItem);
+
+  expect(items.length).toBe(2);
+  expect(getByAltText("Yes")).toBeInTheDocument();
+  expect(previousButton).not.toBeDisabled();
+  expect(nextButton).toBeDisabled();
+});
